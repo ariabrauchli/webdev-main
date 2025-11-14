7b6d538889840eb3b7756c36691d27c80759a3a0
ariabrauchli <42819915+ariabrauchli@users.noreply.github.com>
2025-11-13 23:35:27 -0700

    Update vite.config.js

--- assignment-4/vite.config.js
+++ assignment-4/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/webdev-main/assignment-4/',
-  plugins: [react()]
-});
+  plugins: [react()],
+  base: './',   // important
+})
